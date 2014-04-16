22273fd560946fcdf45f9f5dcfb6e73640d12d14
Stef Busking <stef.busking@gmail.com>
2014-04-16 12:50:50 +0200

    Implement pending Generation tests.

--- test/specs/Generation.tests.js
+++ test/specs/Generation.tests.js
@@ -12,7 +12,7 @@ define(
 		describe('Generation', function() {
 			var generation;
 			beforeEach(function() {
-				generation = new Generation(4);
+				generation = new Generation(10);
 			});
 
 			describe('.reset()', function() {
@@ -40,18 +40,86 @@ define(
 				});
 
 				describe('duplicate', function() {
-					it('joins non-repeating threads');
-					it('discards repeating threads');
-				});
+					var rootThread,
+						forkLeft,
+						forkRight;
+					beforeEach(function() {
+						rootThread = generation.addThread(0);
+						forkLeft = generation.addThread(1, rootThread);
+						forkRight = generation.addThread(2, rootThread);
+					});
 
-				describe('badness scheduling', function() {
-					it('should schedule threads with lower badness before higher ones');
-					it('should schedule threads with higher badness after lower ones');
+					it('joins non-repeating threads', function() {
+						var joiningThread = generation.addThread(2, forkLeft);
+						chai.expect(joiningThread).to.equal(null);
+						chai.expect(forkRight.trace.prefixes.length).to.equal(2);
+					});
+
+					it('discards repeating threads', function() {
+						var repeatingThread = generation.addThread(0, forkLeft);
+						chai.expect(repeatingThread).to.equal(null);
+						chai.expect(rootThread.trace.prefixes.length).to.equal(0);
+					});
 				});
 			});
 
 			describe('.getNextThread()', function() {
-				it('should eventually yield all scheduled threads');
+				function schedule(generation, threadSpecs) {
+					var threads = [];
+					for (var i = 0, l = threadSpecs.length; i < l; ++i) {
+						var pc = threadSpecs[i].pc,
+							parentThread = threads[threadSpecs[i].parent],
+							badness = threadSpecs[i].badness;
+						threads.push(generation.addThread(pc, parentThread, badness));
+					}
+				}
+
+				function expectOrder(generation, threadSpecs) {
+					for (var i = 0, l = threadSpecs.length; i < l; ++i) {
+						var expectedPC = threadSpecs[i].pc,
+							expectedBadness = threadSpecs[i].badness;
+
+						var nextThread = generation.getNextThread();
+						chai.expect(nextThread.pc).to.equal(expectedPC);
+						chai.expect(nextThread.badness).to.equal(expectedBadness);
+					}
+				}
+
+				it('should return threads with lower badness before higher ones', function() {
+					schedule(generation, [
+						{pc: 0, parent: null, badness: 0},
+						{pc: 1, parent: 0, badness: 1},
+						{pc: 2, parent: 0, badness: 0}
+					]);
+					expectOrder(generation, [
+						{pc: 0, badness: 0},
+						{pc: 2, badness: 0},
+						{pc: 1, badness: 1}
+					]);
+				});
+
+				it('should eventually yield all scheduled threads', function() {
+					schedule(generation, [
+						{pc: 0, parent: null, badness: 1},
+						{pc: 1, parent: 0, badness: 2},
+						{pc: 2, parent: 0, badness: 1}
+					]);
+					expectOrder(generation, [
+						{pc: 0, badness: 1}
+					]);
+					schedule(generation, [
+						{pc: 3, parent: null, badness: 0},
+						{pc: 4, parent: 0, badness: 2},
+						{pc: 5, parent: 0, badness: 1}
+					]);
+					expectOrder(generation, [
+						{pc: 3, badness: 0},
+						{pc: 2, badness: 1},
+						{pc: 5, badness: 1},
+						{pc: 1, badness: 2},
+						{pc: 4, badness: 2}
+					]);
+				});
 			});
 		});
 	}
