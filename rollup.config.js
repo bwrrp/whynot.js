21d20af91eb429a20a3914c4651e2887859e56ca
Stef Busking <stef.busking@gmail.com>
2017-05-12 11:32:24 +0200

    Create both UMD and ESM bundles.

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,13 @@
 import babili from 'rollup-plugin-babili';
 
+const { main: MAIN_DEST_FILE, module: MODULE_DEST_FILE } = require('./package.json');
+
 export default {
     entry: 'lib/index.js',
-    dest: 'dist/whynot.min.js',
-    format: 'umd',
+    targets: [
+        { dest: MAIN_DEST_FILE, format: 'umd' },
+        { dest: MODULE_DEST_FILE, format: 'es' },
+    ],
     moduleName: 'whynot',
     exports: 'named',
     sourceMap: true,
