9e48892e031ff3080d2f2039e0340b1acb58e92c
Stef Busking <stef.busking@gmail.com>
2017-09-02 13:45:16 +0200

    Use babel-minify instead of babili

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import babili from 'rollup-plugin-babili';
+import minify from "rollup-plugin-babel-minify";
 
 const { main: MAIN_DEST_FILE, module: MODULE_DEST_FILE } = require('./package.json');
 
@@ -12,7 +12,7 @@ export default {
     exports: 'named',
     sourceMap: true,
     plugins: [
-        babili({
+        minify({
             comments: false,
             sourceMap: true
         })
