69bd7c712a5be10703600e57e992134b6ec19a9a
Stef Busking <stef.busking@gmail.com>
2017-09-19 23:34:06 +0200

    Fix rollup config for 0.50.0

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,19 @@
-import minify from "rollup-plugin-babel-minify";
+import minify from 'rollup-plugin-babel-minify';
 
 const { main: MAIN_DEST_FILE, module: MODULE_DEST_FILE } = require('./package.json');
 
 export default {
-    entry: 'lib/index.js',
-    targets: [
-        { dest: MAIN_DEST_FILE, format: 'umd' },
-        { dest: MODULE_DEST_FILE, format: 'es' },
-    ],
-    moduleName: 'whynot',
-    exports: 'named',
-    sourceMap: true,
-    plugins: [
-        minify({
-            comments: false,
-            sourceMap: true
-        })
-    ]
-}
+	input: 'lib/index.js',
+	output: [
+		{ file: MAIN_DEST_FILE, format: 'umd', exports: 'named' },
+		{ file: MODULE_DEST_FILE, format: 'es' }
+	],
+	name: 'whynot',
+	sourcemap: true,
+	plugins: [
+		minify({
+			comments: false,
+			sourceMap: true
+		})
+	]
+};
