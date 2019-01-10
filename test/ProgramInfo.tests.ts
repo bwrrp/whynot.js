f11c7c8f803934700a1ee7ab43c2bc36b680ca8e
Stef Busking <stef.busking@gmail.com>
2019-01-10 10:59:09 +0100

    Add tests for ProgramInfo validity checks

---
+++ test/ProgramInfo.tests.ts
@@ -0,0 +1,44 @@
+import Assembler from '../src/Assembler';
+import ProgramInfo from '../src/ProgramInfo';
+
+describe('ProgramInfo', () => {
+	let assembler: Assembler<void>;
+	beforeEach(() => {
+		assembler = new Assembler();
+	});
+
+	it('throws when given a program that may run past its end (bad)', () => {
+		assembler.bad();
+		expect(() => {
+			ProgramInfo.fromProgram(assembler.program);
+		}).toThrow('Invalid program: program could run past end');
+	});
+
+	it('throws when given a program that may run past its end (fail)', () => {
+		assembler.fail(() => false);
+		expect(() => {
+			ProgramInfo.fromProgram(assembler.program);
+		}).toThrow('Invalid program: program could run past end');
+	});
+
+	it('throws when given a program that may run past its end (record)', () => {
+		assembler.record('R');
+		expect(() => {
+			ProgramInfo.fromProgram(assembler.program);
+		}).toThrow('Invalid program: program could run past end');
+	});
+
+	it('throws when given a program that may run past its end (jump)', () => {
+		assembler.jump([100]);
+		expect(() => {
+			ProgramInfo.fromProgram(assembler.program);
+		}).toThrow('Invalid program: program could run past end');
+	});
+
+	it('throws when given a program that may run past its end (test)', () => {
+		assembler.test(() => true);
+		expect(() => {
+			ProgramInfo.fromProgram(assembler.program);
+		}).toThrow('Invalid program: program could run past end');
+	});
+});
