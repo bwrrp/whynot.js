972271d2f8c7bb423353283a3798f0986d320c01
Jos Verburg <jos.verburg@liones.nl>
2020-04-24 10:16:14 +0200

    Remove unused variable

--- performance/loadBenchmarks.ts
+++ performance/loadBenchmarks.ts
@@ -1,18 +1,13 @@
 import * as glob from 'glob';
 
 export function getImports(): string[] {
-	const toImport: string[] = [];
 	const files = glob.sync('performance/**/*.benchmark.ts');
-	toImport.push(
-		...files.map(file => {
-			const fileWithoutExtension = file.slice(0, -3);
-			// We need to replace 'performance/' with './'
-			const relativeImport = '.' + fileWithoutExtension.substring('performance'.length);
-			return relativeImport;
-		})
-	);
-
-	return toImport;
+	return files.map(file => {
+		const fileWithoutExtension = file.slice(0, -3);
+		// We need to replace 'performance/' with './'
+		const relativeImport = '.' + fileWithoutExtension.substring('performance'.length);
+		return relativeImport;
+	});
 }
 
 export default async function loadBenchmarks() {
