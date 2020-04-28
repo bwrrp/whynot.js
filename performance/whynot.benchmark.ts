fa34707096e74a0c536e2bec0fd98b703a044561
Stef Busking <stef.busking@gmail.com>
2020-04-28 09:53:21 +0200

    Add comments to explain the benchmarked program

--- performance/whynot.benchmark.ts
+++ performance/whynot.benchmark.ts
@@ -9,14 +9,23 @@ runner.addBenchmark(
 		vm.execute(input);
 	},
 	() => {
+		// Create a program similar to the ones used for completion / synthesis
 		vm = whynot.compileVM(assembler => {
+			// 0: Fork: run program or skip to end
 			assembler.jump([1, 7]);
+			// 1: Fork to either branch
 			assembler.jump([2, 5]);
+			// 2: Branch 1: Accept an input item
 			assembler.test(() => true);
+			// 3: Record acceptance
 			assembler.record('T');
+			// 4: End of branch
 			assembler.jump([6]);
+			// 5: Branch 2: record missing item
 			assembler.record('M');
+			// 6: Join branches, return to start
 			assembler.jump([0]);
+			// 7: End
 			assembler.accept();
 		});
 
